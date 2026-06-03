072e19050fb58c67d879fdc878a31729b6915952
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-03 03:57:17 -0700

    add UserRepository

--- src/main/java/com/learn/attendancemanagementsystem/repository/UserRepository.java
+++ src/main/java/com/learn/attendancemanagementsystem/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.learn.attendancemanagementsystem.repository;
 
-import com.learn.attendancemanagementsystem.model.Role;
+import com.learn.attendancemanagementsystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
