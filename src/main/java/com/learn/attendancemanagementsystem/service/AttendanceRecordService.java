3ea31f79380bbe60576b0967c51e5bfa8e563f51
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-10 03:49:01 -0700

    add AttendanceRecord service

--- src/main/java/com/learn/attendancemanagementsystem/service/AttendanceRecordService.java
+++ src/main/java/com/learn/attendancemanagementsystem/service/AttendanceRecordService.java
@@ -1,5 +1,6 @@
 package com.learn.attendancemanagementsystem.service;
 
+import com.learn.attendancemanagementsystem.model.AttendanceRecord;
 import com.learn.attendancemanagementsystem.repository.AttendanceRecordRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,5 +12,29 @@ public class AttendanceRecordService {
         this.attendanceRecordRepository = attendanceRecordRepository;
     }
 
+    public AttendanceRecord update(Long id, AttendanceRecord newData) {
+        AttendanceRecord attendanceRecord = attendanceRecordRepository.findById(id).get();
 
+        if (attendanceRecord == null) {
+            throw new RuntimeException("AttendanceRecord not found");
+        }
+
+        if (newData.getEntryTime() != null) {
+            attendanceRecord.setEntryTime(newData.getEntryTime());
+        }
+
+        if (newData.getExitTime() != null) {
+            attendanceRecord.setExitTime(newData.getExitTime());
+        }
+
+        if (newData.getUser() != null) {
+            attendanceRecord.setUser(newData.getUser());
+        }
+
+        if (newData.getStatus() != null) {
+            attendanceRecord.setStatus(newData.getStatus());
+        }
+
+        return attendanceRecordRepository.save(attendanceRecord);
+    }
 }
