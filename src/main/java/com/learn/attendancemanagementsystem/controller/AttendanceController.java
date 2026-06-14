23aef36e7dea3d2c4d716d29b2529d3de25dcbe8
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-14 04:13:24 -0700

    Add modified class in the controller Package

--- src/main/java/com/learn/attendancemanagementsystem/controller/AttendanceController.java
+++ src/main/java/com/learn/attendancemanagementsystem/controller/AttendanceController.java
@@ -1,9 +1,11 @@
 package com.learn.attendancemanagementsystem.controller;
 
+import com.learn.attendancemanagementsystem.model.Attendnce;
 import com.learn.attendancemanagementsystem.repository.AttendnceRepository;
 import com.learn.attendancemanagementsystem.service.AttendanceService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("Attendance")
@@ -15,4 +17,25 @@ public class AttendanceController {
         this.attendnceRepository = attendnceRepository;
         this.attendanceService = attendanceService;
     }
+
+    @PostMapping
+    public Attendnce save(@RequestBody Attendnce attendnce) {
+        return attendnceRepository.save(attendnce);
+    }
+
+    @GetMapping
+    public List<Attendnce> attendnces() {
+        return attendnceRepository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long id) {
+       attendnceRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Attendnce update(@RequestBody Attendnce attendnce, @PathVariable("id")Long id) {
+        return attendanceService.updateAttendance(id, attendnce);
+    }
+
 }
