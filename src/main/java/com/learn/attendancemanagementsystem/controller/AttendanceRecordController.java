4fe4f4bcf1798701c4507a6918d48c7d6f39e19e
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-14 03:42:14 -0700

    Add modified class in the controller Package

--- src/main/java/com/learn/attendancemanagementsystem/controller/AttendanceRecordController.java
+++ src/main/java/com/learn/attendancemanagementsystem/controller/AttendanceRecordController.java
@@ -1,7 +1,10 @@
 package com.learn.attendancemanagementsystem.controller;
 
+import com.learn.attendancemanagementsystem.model.AttendanceRecord;
 import com.learn.attendancemanagementsystem.repository.AttendanceRecordRepository;
 import com.learn.attendancemanagementsystem.service.AttendanceRecordService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +14,14 @@ public class AttendanceRecordController {
     private final AttendanceRecordRepository attendanceRecordRepository;
     private final AttendanceRecordService attendanceRecordService;
 
-    public AttendanceRecordController(AttendanceRecordRepository attendanceRecordRepository, AttendanceRecordService attendanceRecordService) {
+    public AttendanceRecordController(AttendanceRecordRepository attendanceRecordRepository
+            , AttendanceRecordService attendanceRecordService) {
         this.attendanceRecordRepository = attendanceRecordRepository;
         this.attendanceRecordService = attendanceRecordService;
+    }
+
+    @PostMapping
+    public AttendanceRecord save(@RequestBody AttendanceRecord attendanceRecord) {
+        return attendanceRecordRepository.save(attendanceRecord);
     }
 }
