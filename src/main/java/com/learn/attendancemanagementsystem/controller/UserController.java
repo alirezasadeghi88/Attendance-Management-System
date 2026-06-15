35fd1c2c2e5050a31a8723ff446c5cb60988a5c3
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-14 23:47:03 -0700

    Add modified class in the controller Package

---
+++ src/main/java/com/learn/attendancemanagementsystem/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.learn.attendancemanagementsystem.controller;
+
+import com.learn.attendancemanagementsystem.repository.UserRepository;
+import com.learn.attendancemanagementsystem.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+    private final UserRepository userRepository;
+    private final UserService userService;
+
+    public UserController(UserRepository userRepository, UserService userService) {
+        this.userRepository = userRepository;
+        this.userService = userService;
+    }
+}
