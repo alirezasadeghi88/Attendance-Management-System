70db41169b9b7e5e31c4faa514c119f9e845d55a
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-22 10:04:01 -0700

    facade packge service class ShiftService

--- src/main/java/com/learn/attendancemanagementsystem/facade/service/ShiftService.java
+++ src/main/java/com/learn/attendancemanagementsystem/facade/service/ShiftService.java
@@ -1,7 +1,13 @@
 package com.learn.attendancemanagementsystem.facade.service;
 
+import com.learn.attendancemanagementsystem.facade.repository.ShiftRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ShiftService {
+    private final ShiftRepository shiftRepository;
+
+    public ShiftService(ShiftRepository shiftRepository) {
+        this.shiftRepository = shiftRepository;
+    }
 }
