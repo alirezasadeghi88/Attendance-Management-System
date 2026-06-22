1a9cc3ed629cdab6a9b3fc9b14f57080caa48051
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-22 10:12:25 -0700

    facade packge service class AttendanceService

--- src/main/java/com/learn/attendancemanagementsystem/facade/service/AttendanceService.java
+++ src/main/java/com/learn/attendancemanagementsystem/facade/service/AttendanceService.java
@@ -1,7 +1,11 @@
 package com.learn.attendancemanagementsystem.facade.service;
 
+import com.learn.attendancemanagementsystem.facade.model.AttendanceRecord;
 import com.learn.attendancemanagementsystem.facade.repository.AttendanceRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Service
 public class AttendanceService {
@@ -10,4 +14,9 @@ public class AttendanceService {
     public AttendanceService(AttendanceRepository attendanceRepository) {
         this.attendanceRepository = attendanceRepository;
     }
-}
+
+    public AttendanceRecord registerCheckIn(Long employeeId, LocalDateTime checkInTime) {
+        LocalDate today = checkInTime.toLocalDate();
+
+
+    }
